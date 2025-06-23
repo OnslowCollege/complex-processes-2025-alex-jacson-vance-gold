1dbd1babbc56889d482a9f5f04c3f8785c270a86
jacson <jacson.gold@student.onslow.school.nz>
2025-06-24 10:34:06 +1200

    Possible fixes with the screen?

--- Project/scripts/os.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode != KEY_ENTER:
-			$Control/Text.text = OS.get_keycode_string(event.keycode)
-			print(OS.get_keycode_string(event.keycode))
