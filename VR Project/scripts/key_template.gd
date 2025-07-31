31b23f9603e67276cedcc4ed9d9500d42233147c
Jacson Gold <jacson.gold@student.onslow.school.nz>
2025-07-31 22:38:52 +1200

    KeyTemplate created, coding still required

---
+++ VR Project/scripts/key_template.gd
@@ -0,0 +1,14 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_area_entered(area: Area3D) -> void:
+	emit_signal("key_pressed", "e")
