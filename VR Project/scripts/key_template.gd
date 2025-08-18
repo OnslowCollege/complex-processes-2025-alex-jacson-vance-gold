2159462d9ae8522a218768982a9d351ea7106da8
Jacson Gold <jacson.gold@student.onslow.school.nz>
2025-08-19 08:36:08 +1200

    Slight keyboard additions, still not working :(

--- VR Project/scripts/key_template.gd
+++ VR Project/scripts/key_template.gd
@@ -1,13 +1,89 @@
-extends Area3D
+extends Node3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+var key_name : String
+
+const key_map = {
+	"`": KEY_QUOTELEFT,
+	"1": KEY_1,
+	"2": KEY_2,
+	"3": KEY_3,
+	"4": KEY_4,
+	"5": KEY_5,
+	"6": KEY_6,
+	"7": KEY_7,
+	"8": KEY_8,
+	"9": KEY_9,
+	"0": KEY_0,
+	"-": KEY_MINUS,
+	"+": KEY_EQUAL,
+	"Backspace": KEY_BACKSPACE,
+	"Tab": KEY_TAB,
+	"q": KEY_Q,
+	"w": KEY_W,
+	"e": KEY_E,
+	"r": KEY_R,
+	"t": KEY_T,
+	"y": KEY_Y,
+	"u": KEY_U,
+	"i": KEY_I,
+	"o": KEY_O,
+	"p": KEY_P,
+	"[": KEY_BRACKETLEFT,
+	"]": KEY_BRACKETRIGHT,
+	"Backslash": KEY_BACKSLASH,
+	"Caps": KEY_CAPSLOCK,
+	"a": KEY_A,
+	"s": KEY_S,
+	"d": KEY_D,
+	"f": KEY_F,
+	"g": KEY_G,
+	"h": KEY_H,
+	"j": KEY_J,
+	"k": KEY_K,
+	"l": KEY_L,
+	";": KEY_SEMICOLON,
+	"Quote": KEY_APOSTROPHE,
+	"Return": KEY_ENTER,
+	"LShift": KEY_SHIFT,
+	"z": KEY_Z,
+	"x": KEY_X,
+	"c": KEY_C,
+	"v": KEY_V,
+	"b": KEY_B,
+	"n": KEY_N,
+	"m": KEY_M,
+	",": KEY_COMMA,
+	".": KEY_PERIOD,
+	"/": KEY_SLASH,
+	"RShift": KEY_SHIFT,
+	"Loptions": KEY_ALT,
+	"Mac": KEY_META,
+	"Spacebar": KEY_SPACE,
+	"Enter": KEY_ENTER,
+	"Roptions": KEY_ALT
+}
+
+func _on_key_pressed(key_name: String) -> void:
+	if key_map.has(key_name):
+		simulate_key_press(key_map[key_name])
+		print("KEY" + str(key_name) + "PRESSED")
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_key_released(key_name: String) -> void:
+	if key_map.has(key_name):
+		simulate_key_release(key_map[key_name])
+	pass # Replace with function body.
 
-func _on_area_entered(area: Area3D) -> void:
-	# emit_signal("key_pressed", "e")
-	pass
+
+func simulate_key_press(keycode: int):
+	var ev := InputEventKey.new()
+	ev.keycode = keycode
+	ev.pressed = true
+	Input.parse_input_event(ev)
+
+
+func simulate_key_release(keycode: int):
+	var ev := InputEventKey.new()
+	ev.keycode = keycode
+	ev.pressed = false
+	Input.parse_input_event(ev)
--- VR Project/scripts/keyboard.gd
+++ VR Project/scripts/keyboard.gd
@@ -11,13 +11,15 @@ const keys = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+", "
 const x_layers = [14, 14, 13, 12, 5]
 const y_layers = 5
 
-func _ready() -> void:
-	var key_template = preload("res://scenes/key_template.tscn")
-	var layer_counter = 2
-	for horiz in range(14):
-		for vert in range(y_layers):
-			var new_key = key_template.instantiate()
-			keys_location.add_child(new_key)
-			new_key.position = Vector3(-1.392 + float(x_layers[0]) * 0.01, 0.315, -0.39 + float(y_layers) * 0.01 )
-			
+#func _ready() -> void:
+#	var key_template = preload("res://scenes/key_template.tscn")
+#	var layer_counter = 2
+#	for horiz in range(14):
+#		for vert in range(y_layers):
+#			var new_key = key_template.instantiate()
+#			keys_location.add_child(new_key)
+#			new_key.position = Vector3(-1.392 + float(x_layers[0]) * 0.01, 0.315, -0.39 + float(y_layers) * 0.01 )
+#			
 	
+func _ready() -> void:
+	$Keys/KeyTemplate.key_name = "a"
