3223d3c600755b1cf885eaeb9ffe3831bd288199
jacson <jacson.gold@student.onslow.school.nz>
2025-09-04 09:58:00 +1200

    os refinement with ghost window implimentation

--- VR Project/scripts/window.gd
+++ VR Project/scripts/window.gd
@@ -9,26 +9,35 @@ var closed = false
 @onready var title_bar: Panel = $Titlebar
 @onready var close: TextureButton = $Titlebar/CloseButton
 
+@onready var ghost_window: Panel = $"../GhostWindow"
 
 # TitleBar dragging code.
 func _ready():
 	title_bar.mouse_filter = Control.MOUSE_FILTER_PASS
 	title_bar.connect("gui_input", _on_title_bar_gui_input)
-	
+	ghost_window.visible = false
 
 func _on_title_bar_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		move_to_front()
+		
 		if event.pressed:
 			dragging = true
 			drag_offset = event.position
+			
+			ghost_window.global_position = global_position
+			ghost_window.size = size
+			ghost_window.visible = true
+			
 		else:
 			dragging = false
+			if ghost_window.visible:
+					global_position = ghost_window.global_position
+					ghost_window.visible = false
+					move_to_front()
 	elif event is InputEventMouseMotion and dragging:
-		global_position += event.relative
+		ghost_window.global_position += event.relative
 
 # Functions that set up
 func _close_window() -> void:
 	hide()
 	closed = true
-	pass # Replace with function body.
