2252506c20b1f2d9acb5b0627d1a764b1092f1f8
AlexConroyWeithaler <alex.conroy-weithaler@student.onslow.school.nz>
2025-09-21 00:14:13 +1200

    Added picture randomizer, added pictures

--- VR Project/scenes/picture.gd
+++ VR Project/scenes/picture.gd
@@ -1,1 +1,14 @@
 extends MeshInstance3D
+
+var textures: Array = [
+load("res://assets/textures/Pictures/coconut.jpg"), 
+load("res://assets/textures/Pictures/hornetdrip.jpg"), 
+load("res://assets/textures/Pictures/miku.jpg"),
+load("res://assets/textures/Pictures/pearto.jpg"),
+load("res://assets/textures/Pictures/monkey.png"),
+load("res://assets/textures/Pictures/penguin.png"),
+load("res://assets/textures/Pictures/luffy.png")]
+
+func _ready():
+	var random_texture = textures[randi() % textures.size()]
+	self.mesh.material.albedo_texture = random_texture
