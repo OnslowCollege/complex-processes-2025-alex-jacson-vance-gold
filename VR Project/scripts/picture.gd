361668d59c1b16609a95693e1bb2bd51c639b000
AlexConroyWeithaler <alex.conroy-weithaler@student.onslow.school.nz>
2025-09-21 13:26:30 +1200

    Cleanup, made the picture texture thing modular like the rest

--- VR Project/scripts/picture.gd
+++ VR Project/scripts/picture.gd
@@ -1,14 +1,26 @@
 extends MeshInstance3D
 
-var textures: Array = [
-load("res://assets/textures/Pictures/coconut.jpg"), 
-load("res://assets/textures/Pictures/hornetdrip.jpg"), 
-load("res://assets/textures/Pictures/miku.jpg"),
-load("res://assets/textures/Pictures/pearto.jpg"),
-load("res://assets/textures/Pictures/monkey.png"),
-load("res://assets/textures/Pictures/penguin.png"),
-load("res://assets/textures/Pictures/luffy.png")]
+func get_image_files_in_folder(path) -> Array:
+	var image_files_array = []
+	var dir = DirAccess.open(path)
 
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir(): # Check if it's a file, not a directory
+				var extension = file_name.get_extension().to_lower()
+				# Add common image file extensions here
+				if extension in ["png", "jpg", "jpeg", "webp", "bmp", "tga", "svg"]:
+					image_files_array.append(path + "/" + file_name)
+			file_name = dir.get_next()
+		dir.list_dir_end()
+	else:
+		print("Could not open directory: " + path)
+
+	return image_files_array
 func _ready():
+	var textures = get_image_files_in_folder("res://assets/textures/Pictures/")
 	var random_texture = textures[randi() % textures.size()]
+	random_texture = load(random_texture)
 	self.mesh.material.albedo_texture = random_texture
