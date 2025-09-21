4d4669e67d6997fd3e9384018a6fcbe997a61ba9
Jacson Gold <jacson.gold@student.onslow.school.nz>
2025-09-21 22:09:35 +1200

    Sprint 6 done maybe idk

--- VR Project/scripts/dropdown_button_template.gd
+++ VR Project/scripts/dropdown_button_template.gd
@@ -2,9 +2,13 @@ extends Button
 
 @export var connected_app: String
 
+@onready var top_bar_button: BaseButton = $"../../.."
+
+
 func _ready():
 	# Connect the button's "pressed" signal to a function
 	self.pressed.connect(_on_my_button_pressed)
 
 func _on_my_button_pressed():
 	Globals.open_app(self.connected_app)
+	top_bar_button.button_pressed = false
