c844ca05dd5852ed072d568e219fe19a20dd016d
Jacson Gold <jacson.gold@student.onslow.school.nz>
2025-10-15 19:18:44 +1300

    lil error sfx when non-functioning app works

--- VR Project/scripts/dropdown_button_template.gd
+++ VR Project/scripts/dropdown_button_template.gd
@@ -1,6 +1,7 @@
 extends Button
 
 @export var connected_app: String
+@export var has_functionality: bool
 
 @onready var top_bar_button: BaseButton = $"../../.."
 
@@ -10,5 +11,9 @@ func _ready():
 	self.pressed.connect(_on_my_button_pressed)
 
 func _on_my_button_pressed():
+	
+	if has_functionality == false: Globals.signal_chime()
+	else: pass
+	
 	Globals.open_app(self.connected_app)
 	top_bar_button.button_pressed = false
